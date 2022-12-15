2faa54a0d4dcbeb6e909338e6f34df816219f186
jessica coueron <jcoueron@yahoo.fr>
2022-12-15 19:03:19 +0100

    fix controller stupid coaching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,11 @@ class PagesController < ApplicationController
   end
 
   def answer
-  #conditions en fonction de if params[:ask] = "hello" @answer = "answer"
-    if params[:ask] =="I am going to work"
+    @question = params[:question]
+  #conditions en fonction de if params[:question] = "hello" @answer = "answer"
+    if params[:question] =="I am going to work"
       @answer = "Great"
-    elsif params[:ask].include?("?")
+    elsif params[:question].include?("?")
       @answer = "Silly question, get dressed and go to work"
     else
       @answer = "I don't care, get dressed and go to work!"
