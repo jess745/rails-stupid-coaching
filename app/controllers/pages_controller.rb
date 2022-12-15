6e66aa1880db649b9f53182527c29de274e420e4
jessica coueron <jcoueron@yahoo.fr>
2022-12-15 18:42:04 +0100

    stupid caoaching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     if params[:ask] =="I am going to work"
       @answer = "Great"
     elsif params[:ask].include?("?")
-      @answer = "Silly question, get dressed and goto work"
+      @answer = "Silly question, get dressed and go to work"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
